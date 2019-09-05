5958fb8a1342edb7d7915283f1ab25e6060229ef
SanjoyKH <sanjoykh78.x1@gmail.com>
2019-09-05 20:08:42 +0530

    MInor SEtup Changes

--- app/src/main/java/com/example/skhmyapplication1/MainActivity.kt
+++ app/src/main/java/com/example/skhmyapplication1/MainActivity.kt
@@ -21,8 +21,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText1.setOnClickListener {
-           /* val dateStr = "12/3/2017"
+        /*  editText1.setOnClickListener {
+             *//* val dateStr = "12/3/2017"
             val dateStr2 = "12/4/2017"
             val sdf= SimpleDateFormat("MM/dd/yyyy")
             val date = sdf.parse(dateStr)
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
             val diffInMillies = date2.time - date.time
             val diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS)+1
-            textView1.text= diff.toString()*/
+            textView1.text= diff.toString()*//*
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
@@ -47,9 +47,9 @@ class MainActivity : AppCompatActivity() {
             }, year, month,day)
             dpd.show()
 
-        }
-        editText2.setOnClickListener {
-            /* val dateStr = "12/3/2017"
+        }*/
+      /*  editText2.setOnClickListener {
+            *//* val dateStr = "12/3/2017"
              val dateStr2 = "12/4/2017"
              val sdf= SimpleDateFormat("MM/dd/yyyy")
              val date = sdf.parse(dateStr)
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity() {
 
              val diffInMillies = date2.time - date.time
              val diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS)+1
-             textView1.text= diff.toString()*/
+             textView1.text= diff.toString()*//*
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
@@ -74,9 +74,9 @@ class MainActivity : AppCompatActivity() {
             }, year, month,day)
             dpd.show()
 
-        }
-        button1.setOnClickListener {
-            /* val dateStr = "12/3/2017"
+        }*/
+     /*   button1.setOnClickListener {
+            *//* val dateStr = "12/3/2017"
              val dateStr2 = "12/4/2017"
              val sdf= SimpleDateFormat("MM/dd/yyyy")
              val date = sdf.parse(dateStr)
@@ -84,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 
              val diffInMillies = date2.time - date.time
              val diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS)+1
-             textView1.text= diff.toString()*/
+             textView1.text= diff.toString()*//*
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
@@ -101,7 +101,7 @@ class MainActivity : AppCompatActivity() {
             }, year, month,day)
             dpd.show()
 
-        }
+        }*/
         textView1.setOnClickListener {
             /* val dateStr = "12/3/2017"
              val dateStr2 = "12/4/2017"
@@ -123,10 +123,38 @@ class MainActivity : AppCompatActivity() {
                 val date="$dayOfMonth - ${monthOfYear + 1} - $year"
 
                 textView1.text= date
-                editText1.setText(date)
+              //  editText1.setText(date)
                 // textView1.toString(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG)
             }, year, month,day)
             dpd.show()
+
+        }
+        textView2.setOnClickListener {
+            /* val dateStr = "12/3/2017"
+             val dateStr2 = "12/4/2017"
+             val sdf= SimpleDateFormat("MM/dd/yyyy")
+             val date = sdf.parse(dateStr)
+             val date2 = sdf.parse(dateStr2)
+
+             val diffInMillies = date2.time - date.time
+             val diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS)+1
+             textView1.text= diff.toString()*/
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in Toast
+                Toast.makeText(this, """$dayOfMonth/${monthOfYear + 1}/$year""", Toast.LENGTH_LONG).show()
+                val date="$dayOfMonth - ${monthOfYear + 1} - $year"
+
+                textView2.text= date
+                //editText1.setText(date)
+                // textView1.toString(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG)
+            }, year, month,day)
+            dpd.show()
+
 
         }
     }
